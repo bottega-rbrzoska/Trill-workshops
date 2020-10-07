15a1272dc5a6f8967d62aa03c0eedec4035b1542
Piotr Gankiewicz <piotr.gankiewicz@gmail.com>
2020-10-07 10:25:57 +0200

    Core

---
+++ src/Trill.Core/Entities/Story.cs
@@ -0,0 +1,27 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using Trill.Core.ValueObjects;
+
+namespace Trill.Core.Entities
+{
+    public class Story
+    {
+        public Guid Id { get; private set; }
+        public string Title { get; private set; }
+        public string Text { get; private set; }
+        public Author Author { get; private set; }
+        public IEnumerable<string> Tags { get; private set; }
+        public DateTime CreatedAt { get; private set; }
+
+        public Story(Guid id, string title, string text, Author author, IEnumerable<string> tags, DateTime createdAt)
+        {
+            Id = id;
+            Title = title.Trim();
+            Text = text.Trim();
+            Author = author;
+            Tags = tags ?? Enumerable.Empty<string>();
+            CreatedAt = createdAt;
+        }
+    }
+}
---
+++ src/Trill.Core/ValueObjects/Author.cs
@@ -0,0 +1,39 @@
+using System;
+
+namespace Trill.Core.ValueObjects
+{
+    public class Author : IEquatable<Author>
+    {
+        public string Name { get; }
+
+        public Author(string name)
+        {
+            if (string.IsNullOrWhiteSpace(name))
+            {
+                throw new ArgumentException(name);
+            }
+        
+            Name = name;
+        }
+        
+        public static implicit operator Author(string author) => new Author(author);
+
+        public static implicit operator string(Author author) => author.Name;
+
+        public bool Equals(Author other)
+        {
+            if (ReferenceEquals(null, other)) return false;
+            if (ReferenceEquals(this, other)) return true;
+            return Name == other.Name;
+        }
+
+        public override bool Equals(object obj)
+        {
+            if (ReferenceEquals(null, obj)) return false;
+            if (ReferenceEquals(this, obj)) return true;
+            return obj.GetType() == GetType() && Equals((Author) obj);
+        }
+
+        public override int GetHashCode() => Name != null ? Name.GetHashCode() : 0;
+    }
+}
