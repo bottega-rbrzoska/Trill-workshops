d0ef2752ecd88a1cdffcbf2bbd8a07b8e376160d
Piotr Gankiewicz <piotr.gankiewicz@gmail.com>
2020-10-07 15:17:11 +0200

    Application

--- src/Trill.Application/Services/IStoryService.cs
+++ src/Trill.Application/Services/IStoryService.cs
@@ -1,6 +1,8 @@
 using System;
 using System.Collections.Generic;
 using System.Threading.Tasks;
+using Trill.Application.Commands;
+using Trill.Application.DTO;
 
 namespace Trill.Application.Services
 {
