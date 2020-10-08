a759eca0395b389835f78f55c85495aca69bae85
Piotr Gankiewicz <piotr.gankiewicz@gmail.com>
2020-10-08 15:20:37 +0200

    integration tests

--- tests/Trill.Tests.Integration/Controllers/StoriesControllerTests.cs
+++ tests/Trill.Tests.Integration/Controllers/StoriesControllerTests.cs
@@ -1,10 +1,13 @@
 using System;
 using System.Net;
 using System.Net.Http;
+using System.Text;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc.Testing;
+using Newtonsoft.Json;
 using Shouldly;
 using Trill.Api;
+using Trill.Application.Commands;
 using Xunit;
 
 namespace Trill.Tests.Integration.Controllers
@@ -18,6 +21,23 @@ namespace Trill.Tests.Integration.Controllers
             var response = await _client.GetAsync($"api/stories/{storyId}");
             response.StatusCode.ShouldBe(HttpStatusCode.NotFound);
         }
+
+        [Fact]
+        public async Task post_send_story_should_return_created_and_location_header()
+        {
+            var command = new SendStory(Guid.NewGuid(), "test", "Lorem ipsum",
+                "test_user", new[] {"tag1", "tag2"});
+            var payload = GetPayload(command);
+            
+            var response = await _client.PostAsync("api/stories", payload);
+            response.StatusCode.ShouldBe(HttpStatusCode.Created);
+            var location = response.Headers.Location;
+            location.ShouldNotBeNull();
+            location.ToString().ShouldBe($"http://localhost/api/Stories/{command.Id}");
+        }
+
+        private static StringContent GetPayload<T>(T value)
+            => new StringContent(JsonConvert.SerializeObject(value), Encoding.UTF8, "application/json");
         
         #region Arrange
         
