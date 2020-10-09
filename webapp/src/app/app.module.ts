a42be00208ef4fdf39de88f6c46be11b86d0b04b
Rafal Brzoska <rbrzoska@future-processing.com>
2020-10-09 10:43:08 +0200

    Routing

--- webapp/src/app/app.module.ts
+++ webapp/src/app/app.module.ts
@@ -3,10 +3,18 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomeComponent } from './home/home.component';
+import { PageNotFoundComponent } from './page-not-found/page-not-found.component';
+import { ContactComponent } from './contact/contact.component';
+import { NavigationComponent } from './navigation/navigation.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    PageNotFoundComponent,
+    ContactComponent,
+    NavigationComponent
   ],
   imports: [
     BrowserModule,
